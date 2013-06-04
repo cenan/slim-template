128748dfa34837993e62a6ceaab6be306ae5f388
cenan <me@cenanozen.com>
2013-06-04 23:14:40 +0300

    created composer.json and public index.php

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+require '../vendor/autoload.php';
+
+// Prepare app
+$app = new \Slim\Slim(array(
+    'templates.path' => '../templates',
+    'log.level' => 4,
+    'log.enabled' => true,
+    'log.writer' => new \Slim\Extras\Log\DateTimeFileWriter(array(
+        'path' => '../logs',
+        'name_format' => 'y-m-d'
+    ))
+));
+
+// Prepare view
+\Slim\Extras\Views\Twig::$twigOptions = array(
+    'charset' => 'utf-8',
+    'cache' => realpath('../templates/cache'),
+    'auto_reload' => true,
+    'strict_variables' => false,
+    'autoescape' => true
+);
+$app->view(new \Slim\Extras\Views\Twig());
+
+// Define routes
+$app->get('/', function () use ($app) {
+    $app->render('index.html');
+});
+
+// Run app
+$app->run();
