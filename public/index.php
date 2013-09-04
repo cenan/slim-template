4ce2234d7ae857a65617078ca53c3f59da87cee8
cenan <me@cenanozen.com>
2013-09-04 21:39:16 +0300

    cleanup

--- public/index.php
+++ public/index.php
@@ -5,28 +5,28 @@ date_default_timezone_set("America/Los_Angeles");
 require '../vendor/autoload.php';
 
 // Prepare app
-$app = new \Slim\Slim(array(
+$app = new \Slim\Slim([
     'templates.path' => '../app/views',
     'log.level' => 4,
     'log.enabled' => true,
-    'log.writer' => new \Slim\Extras\Log\DateTimeFileWriter(array(
+    'log.writer' => new \Slim\Extras\Log\DateTimeFileWriter([
         'path' => '../logs',
         'name_format' => 'y-m-d'
-    ))
-));
+    ])
+]);
 
 // Prepare view
 $view = new \Slim\Views\Twig();
-$view->parserOptions = array(
+$view->parserOptions = [
     'charset' => 'utf-8',
     'cache' => realpath('../app/views/cache'),
     'auto_reload' => true,
     'strict_variables' => false,
     'autoescape' => true
-);
-$view->parserExtensions = array(
+];
+$view->parserExtensions = [
     new \Slim\Views\TwigExtension(),
-);
+];
 $app->view($view);
 
 $app->notFound(function () use ($app) {
